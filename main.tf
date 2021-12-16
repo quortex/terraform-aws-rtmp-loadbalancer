1036f156eca195e3320c2d58146db39073d71965
Vincent Marguerie <v.marguerie@quortex.io>
2021-12-16 09:45:00 +0100

    add iac for rtmp loadbalancer

---
+++ main.tf
@@ -0,0 +1,226 @@
+data "aws_elb_service_account" "current" {}
+
+# The elastic loadbalancer for rtmp.
+locals {
+  elb_name = var.elb_name != "" ? var.elb_name : var.name
+}
+resource "aws_elb" "rtmp" {
+  name                        = local.elb_name
+  subnets                     = var.subnet_ids
+  security_groups             = [aws_security_group.rtmp_loadbalancer.id]
+  cross_zone_load_balancing   = var.elb_cross_zone_load_balancing
+  idle_timeout                = var.elb_idle_timeout
+  connection_draining         = var.elb_connection_draining
+  connection_draining_timeout = var.elb_connection_draining_timeout
+
+  # Access logs storage in s3
+  access_logs {
+    enabled       = var.access_logs_enabled
+    bucket        = aws_s3_bucket.access_logs.bucket
+    bucket_prefix = var.access_logs_bucket_prefix
+    interval      = var.access_logs_interval
+  }
+
+  # The RTMP listener configuration
+  listener {
+    instance_port     = var.rtmp_backend_ingress_port
+    instance_protocol = "tcp"
+    lb_port           = 1935
+    lb_protocol       = "tcp"
+  }
+
+  # The RTMPS listener configuration
+  dynamic "listener" {
+    for_each = var.rtmps_enabled ? [true] : []
+
+    content {
+      instance_port      = var.rtmp_backend_ingress_port
+      instance_protocol  = "tcp"
+      lb_port            = 443
+      lb_protocol        = "ssl"
+      ssl_certificate_id = aws_acm_certificate.cert.0.id
+    }
+  }
+
+  # Health check configuration
+  health_check {
+    healthy_threshold   = var.elb_health_check_healthy_threshold
+    unhealthy_threshold = var.elb_health_check_unhealthy_threshold
+    timeout             = var.elb_health_check_timeout
+    target              = "TCP:${var.rtmp_backend_ingress_port}"
+    interval            = var.elb_health_check_interval
+  }
+
+  tags = var.tags
+}
+
+# ELB target groups attachment
+resource "aws_autoscaling_attachment" "rtmp" {
+  autoscaling_group_name = var.rtmp_backend_autoscaling_group_name
+  elb                    = aws_elb.rtmp.name
+}
+
+# ELB security group configuration
+locals {
+  sg_name = var.elb_security_group_name != "" ? var.elb_security_group_name : "${var.name}-elb"
+}
+resource "aws_security_group" "rtmp_loadbalancer" {
+  name        = local.sg_name
+  description = "Security group for the rtmp ELB ${local.elb_name}"
+  vpc_id      = var.vpc_id
+
+  tags = merge({
+    Name = local.sg_name
+    },
+    var.tags
+  )
+}
+
+resource "aws_security_group_rule" "rtmp_loadbalancer_ingress_rtmp" {
+  count             = length(var.elb_ingress_cidr_blocks_rtmp) > 0 ? 1 : 0
+  description       = "Allow RTMP ingress"
+  type              = "ingress"
+  from_port         = 1935
+  to_port           = 1935
+  protocol          = "tcp"
+  cidr_blocks       = var.elb_ingress_cidr_blocks_rtmp
+  security_group_id = aws_security_group.rtmp_loadbalancer.id
+}
+
+resource "aws_security_group_rule" "rtmp_loadbalancer_ingress_rtmps" {
+  count             = length(var.elb_ingress_cidr_blocks_rtmps) > 0 ? 1 : 0
+  description       = "Allow RTMPS ingress"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = var.elb_ingress_cidr_blocks_rtmps
+  security_group_id = aws_security_group.rtmp_loadbalancer.id
+}
+
+resource "aws_security_group_rule" "rtmp_loadbalancer_egress" {
+  description       = "Allow all traffic out"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rtmp_loadbalancer.id
+}
+
+# ELB backend ingress permissions
+resource "aws_security_group_rule" "rtmp_ingress" {
+  description       = "Allow access to the rtmp ingress port from the VPC"
+  security_group_id = var.rtmp_backend_security_group_id
+  protocol          = "tcp"
+  type              = "ingress"
+  from_port         = var.rtmp_backend_ingress_port
+  to_port           = var.rtmp_backend_ingress_port
+  cidr_blocks       = [var.vpc_cidr]
+}
+
+# ELB access logs bucket configuration
+resource "aws_s3_bucket" "access_logs" {
+  bucket        = var.access_logs_bucket_name != "" ? var.access_logs_bucket_name : "${var.name}-access-logs"
+  force_destroy = var.access_logs_force_destroy
+  acl           = "private"
+
+  lifecycle_rule {
+    enabled = var.access_logs_expiration != null
+    expiration {
+      days = var.access_logs_expiration
+    }
+    tags = var.tags
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_policy" "access_logs" {
+  bucket = aws_s3_bucket.access_logs.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_elb_service_account.current.id}:root"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.access_logs.bucket}/*"
+      },
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.access_logs.bucket}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      },
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "delivery.logs.amazonaws.com"
+        },
+        Action   = "s3:GetBucketAcl",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.access_logs.bucket}"
+      }
+    ]
+  })
+}
+
+# DNS record alias that target the load balancer
+resource "aws_route53_record" "rtmp" {
+  zone_id = var.dns_hosted_zone_id
+  name    = var.dns_record
+  type    = "A"
+
+  alias {
+    name                   = aws_elb.rtmp.dns_name
+    zone_id                = aws_elb.rtmp.zone_id
+    evaluate_target_health = false
+  }
+}
+
+# Certificate in AWS Certificate Manager
+locals {
+  cert_name   = var.ssl_certificate_name != "" ? var.ssl_certificate_name : var.name
+  create_cert = var.ssl_certificate_arn == null && var.rtmps_enabled
+}
+resource "aws_acm_certificate" "cert" {
+  count             = local.create_cert ? 1 : 0
+  domain_name       = var.ssl_certificate_domain_name
+  validation_method = "DNS"
+
+  tags = merge({
+    Name = local.cert_name
+    },
+    var.tags
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# DNS record to validate this certificate
+resource "aws_route53_record" "cert_validation" {
+  count   = local.create_cert ? 1 : 0
+  name    = tolist(aws_acm_certificate.cert.0.domain_validation_options).0.resource_record_name
+  type    = tolist(aws_acm_certificate.cert.0.domain_validation_options).0.resource_record_type
+  zone_id = var.dns_hosted_zone_id
+  records = [tolist(aws_acm_certificate.cert.0.domain_validation_options).0.resource_record_value]
+  ttl     = 60
+}
+
+# Certificate validation
+resource "aws_acm_certificate_validation" "cert" {
+  count                   = local.create_cert ? 1 : 0
+  certificate_arn         = aws_acm_certificate.cert.0.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.0.fqdn]
+}
---
+++ variables.tf
@@ -0,0 +1,184 @@
+variable "name" {
+  type        = string
+  description = "A name from which the name of the resources will be chosen. Note that each resource name can be set individually."
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC in which the resources should be deployed."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The CIDR for the VPC."
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "The IDs of the subnets where resources should be placed."
+  default     = []
+}
+
+variable "elb_name" {
+  type        = string
+  description = "Override the ELB name."
+  default     = ""
+}
+
+variable "elb_cross_zone_load_balancing" {
+  type        = bool
+  description = "Enable cross-zone load balancing."
+  default     = false
+}
+
+variable "elb_idle_timeout" {
+  type        = number
+  description = "The time in seconds that the connection is allowed to be idle."
+  default     = 60
+}
+
+variable "elb_connection_draining" {
+  type        = bool
+  description = "Boolean to enable connection draining."
+  default     = true
+}
+
+variable "elb_connection_draining_timeout" {
+  type        = number
+  description = "The time in seconds to allow for connections to drain."
+  default     = 300
+}
+
+variable "elb_health_check_healthy_threshold" {
+  type        = number
+  description = "The number of checks before the instance is declared healthy."
+  default     = 6
+}
+
+variable "elb_health_check_unhealthy_threshold" {
+  type        = number
+  description = "The number of checks before the instance is declared unhealthy."
+  default     = 2
+}
+
+variable "elb_health_check_timeout" {
+  type        = number
+  description = "The interval between checks."
+  default     = 5
+}
+
+variable "elb_health_check_interval" {
+  type        = number
+  description = "The length of time before the check times out."
+  default     = 10
+}
+
+variable "elb_security_group_name" {
+  type        = string
+  description = "Override the ELB security group name."
+  default     = ""
+}
+
+variable "elb_ingress_cidr_blocks_rtmp" {
+  type        = list(string)
+  description = "CIDRs to allow for the rtmp ingress."
+  default     = ["0.0.0.0/0"]
+}
+
+variable "elb_ingress_cidr_blocks_rtmps" {
+  type        = list(string)
+  description = "CIDRs to allow for the rtmps ingress."
+  default     = ["0.0.0.0/0"]
+}
+
+variable "rtmp_backend_ingress_port" {
+  type        = string
+  description = "The rtmp backend ingress port (envoy port for rtmp)."
+}
+
+variable "rtmp_backend_security_group_id" {
+  type        = string
+  description = "The rtmp backend security group id (used to allow ingress on rtmp_backend_ingress_port)."
+}
+
+variable "rtmp_backend_autoscaling_group_name" {
+  type        = string
+  description = "The rtmp backend ASG name."
+}
+
+variable "access_logs_enabled" {
+  type        = bool
+  description = "Wether to enable elb access logs or not."
+  default     = false
+}
+
+variable "access_logs_bucket_name" {
+  type        = string
+  description = "Override the access logs bucket name."
+  default     = ""
+}
+
+variable "access_logs_bucket_prefix" {
+  type        = string
+  description = "The access logs bucket prefix. Logs are stored in the root if not configured."
+  default     = null
+}
+
+variable "access_logs_interval" {
+  type        = number
+  description = "The publishing interval in minutes."
+  default     = 60
+}
+
+variable "access_logs_expiration" {
+  type        = number
+  description = "Specifies the number of days for which access logs are kept (indefinitely if not specified)."
+  default     = null
+}
+
+variable "access_logs_force_destroy" {
+  type        = bool
+  description = "A boolean that indicates all objects should be deleted from the access logs bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  default     = false
+}
+
+variable "rtmps_enabled" {
+  type        = bool
+  description = "Wether to enable rtmps. If set to true, a certificate will be created in certificate manager as well as load balancer configuration to perform ssl termination."
+  default     = true
+}
+
+variable "dns_hosted_zone_id" {
+  type        = string
+  description = "The ID of the hosted zone in Route53, under which the DNS record should be created."
+}
+
+variable "dns_record" {
+  type        = string
+  description = "The domain name record to add in zone defined by dns_hosted_zone_id for alias on elb dns name."
+  default     = "rtmp"
+}
+
+variable "ssl_certificate_arn" {
+  type        = string
+  description = "The ARN identifier of an existing Certificate in AWS Certificate Manager, to be used for RTMPS requests. If not defined, a new certificate will be issued and validated in the AWS Certificate Manager."
+  default     = null
+}
+
+variable "ssl_certificate_name" {
+  type        = string
+  description = "Override the cert manager certificate name."
+  default     = ""
+}
+
+variable "ssl_certificate_domain_name" {
+  type        = string
+  description = "The complete domain name that will be written in the TLS certificate. Can include a wildcard. Required for rtmps."
+  default     = null
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to apply to resources. A list of key->value pairs."
+  default     = {}
+}
