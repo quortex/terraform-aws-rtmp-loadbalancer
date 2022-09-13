55b391cdee1ada5e4c91061c74710bf0912ee758
swbsf <s.wallimann@quortex.io>
2022-09-13 12:26:02 +0200

    Adding encryption option for buckets

--- main.tf
+++ main.tf
@@ -139,6 +139,18 @@ resource "aws_s3_bucket_public_access_block" "access_logs" {
   restrict_public_buckets = true
 }
 
+# Set minimal encryption on buckets
+resource "aws_s3_bucket_server_side_encryption_configuration" "access_logs" {
+  count  = var.enable_bucket_encryption ? 1 : 0
+  bucket = aws_s3_bucket.access_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "access_logs" {
   bucket = aws_s3_bucket.access_logs.id
   policy = jsonencode({
