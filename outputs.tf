518a22b05785e49a9e36c64657eeae801974266a
Antonin <9219052+antonincms@users.noreply.github.com>
2024-07-29 15:50:44 +0200

    Switch module from Classic Load Balancer to Network Load Balancer (#7)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "dns_record" {
   description = "The DNS record for the RTMP endpoint"
   value       = aws_route53_record.rtmp.fqdn
 }
+
+output "lb_target_group_arn" {
+  value       = aws_lb_target_group.rtmp.arn
+  description = "The ARN of the target group of the RTMP load balancer."
+}
